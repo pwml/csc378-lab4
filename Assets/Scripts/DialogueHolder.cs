324ec90e712d72aa6d02cfa805af85fc69ca567e
akfanelli <akfanell@calpoly.edu>
2023-02-20 18:22:42 -0800

    dialogue box works now

---
+++ Assets/Scripts/DialogueHolder.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DialogueHolder : MonoBehaviour
+{
+    public string dialogue;
+    private DialogueManager dMan;
+    private bool playerEnter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dMan = FindObjectOfType<DialogueManager>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.E) && playerEnter && !dMan.dialogueActive)
+        {
+            dMan.ShowBox(dialogue);
+        }
+        else if (Input.GetKeyUp(KeyCode.E) && playerEnter && dMan.dialogueActive)
+        {
+            dMan.HideBox();
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.name == "Bear")
+        {
+            playerEnter = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Bear")
+        {
+            dMan.HideBox();
+            playerEnter = false;
+        }
+    }
+
+    // public string dialogue;
+    // private DialogueManager dMan;
+
+    // // Start is called before the first frame update
+    // void Start()
+    // {
+    //     dMan = FindObjectOfType<DialogueManager>();    
+    // }
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+        
+    // }
+
+    // void OnTriggerStay2D(Collider2D other)
+    // {
+    //     if(other.gameObject.name == "Bear")
+    //     {
+    //         if (Input.GetKeyUp(KeyCode.E))
+    //         {
+    //             dMan.ShowBox(dialogue);
+    //         }
+    //     }
+    // }
+}
---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DialogueManager : MonoBehaviour
+{
+    public GameObject dBox;
+    public TextMeshProUGUI dText;
+    public bool dialogueActive;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ShowBox(string dialogue)
+    {
+        dialogueActive = true;
+        dBox.SetActive(true);
+        dText.text = dialogue;
+    }
+
+    public void HideBox()
+    {
+        dialogueActive = false;
+        dBox.SetActive(false);
+    }
+
+    // public GameObject dBox;
+    // public TextMeshProUGUI dText;
+    // public bool dialogueActive;
+
+    // // Start is called before the first frame update
+    // void Start()
+    // {
+        
+    // }
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+    //     if (dialogueActive && Input.GetKey(KeyCode.E))
+    //     {
+    //         dBox.SetActive(false);
+    //         dialogueActive = false;
+    //     }
+    // }
+
+    // public void ShowBox(string dialogue)
+    // {
+    //     dialogueActive = true;
+    //     dBox.SetActive(true);
+    //     dText.text = dialogue;
+    // }
+}
