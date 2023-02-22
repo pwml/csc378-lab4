10cc37252513e82f27ede9b85bcea7ff09c07fea
akfanelli <akfanell@calpoly.edu>
2023-02-21 17:52:48 -0800

    destroy gameobject after eating

--- Assets/Scripts/EatingTrigger.cs
+++ Assets/Scripts/EatingTrigger.cs
@@ -7,38 +7,20 @@ using UnityEngine.Events;
 public class EatingTrigger : MonoBehaviour
 {
     public Animator animator;
-    // [Header("CustomEvent")]
-    // public UnityEvent eatingEvent;
+    public GameObject gameObject;
     private bool playerEnter;
 
     public AudioSource Eating;
 
     void Update() {
-        // print(GetComponent<Animator>().GetCurrentAnimatorStateInfo(0));
         if (Input.GetKeyDown(KeyCode.E) && playerEnter){
             animator.SetTrigger("isBeingEaten");
             Eating.Play();
-            // try {
-            //     print("Event triggers.");
-            //     eatingEvent.Invoke();
-            // }       
-            // catch {
-            //     print("No event to trigger.");
-            //     Debug.Log("No event to trigger.");
-            // }
+            if (gameObject.name == "Beehive" || gameObject.name == "Beehive" || gameObject.name == "Salmon"){
+                Destroy(gameObject, 3);
+            }
         }
     }
-    
-    // private void eatingTrigger(Collider2D other){
-    //     try {
-    //         print("Event triggers.");
-    //         eatingEvent.Invoke();
-    //     }       
-    //     catch {
-    //         print("No event to trigger.");
-    //         Debug.Log("No event to trigger.");
-    //     }
-    // }
 
     void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.name == "Bear"){
